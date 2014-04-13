7a6398fd1874c50d5c9dee6183da2ec7a9988e77
Louis Stanard <louis.stanard@gmail.com>
2014-04-13 18:45:48 -0500

    Initial commit
    Initial commit of site files

---
+++ _js/_init.js
@@ -0,0 +1,28 @@
+/*------------------------------------------------------------------
+    INITIALIZERS: UI components
+-------------------------------------------------------------------*/
+
+var functionCheck = jRespond([
+		{
+			label: 'med-lrg',
+			enter: 648,
+			exit: 10000
+		}
+	]);
+
+	functionCheck.addFunc({
+		breakpoint: '*',
+		enter: function() {
+			uiFunctions.site.setFooterYear();
+		}
+	});
+
+	functionCheck.addFunc({
+		breakpoint: 'med-lrg',
+		enter: function() {
+			uiFunctions.site.setColumnHeight(['.featured-products__grid li', '.featured-recipes__grid ul li']);
+		},
+		exit: function() {
+			uiFunctions.site.unsetColumnHeight(['.featured-products__grid li', '.featured-recipes__grid ul li']);
+		}
+	});
---
+++ _js/_ui/_ui.js
@@ -0,0 +1,91 @@
+/*------------------------------------------------------------------
+    USER INTERFACE COMPONENTS
+-------------------------------------------------------------------*/
+
+(function() {
+
+	var $w = $(window),
+		sw = document.documentElement.clientWidth,
+		sh = document.documentElement.clientHeight;
+
+		$w.smartresize(function(){
+			sw = document.documentElement.clientWidth;
+			sh = document.documentElement.clientHeight;
+			console.log(sw, sh);
+		});
+
+		return uiFunctions = {
+
+			site: {
+
+				setFooterYear: function() {
+					var year = new Date().getFullYear();
+					$('#footer__year').text(year);
+				},
+
+				unsetColumnHeight: function(selectorArray) {
+
+					var selectors = new Array();
+
+					$.each(selectorArray, function(index, value){
+						selectors.push($(value));
+					});
+
+					$.each(selectors, function() {
+						$(this).css('height', '');
+					});
+
+				},
+
+				setColumnHeight: function(selectorArray) {
+
+					var selectors = new Array(),
+						currentTallest = 0,
+						currentRowStart = 0,
+						rowDivs = new Array(),
+						$el,
+						topPosition = 0;
+
+					$.each(selectorArray, function(index, value){
+						selectors.push($(value));
+					});
+
+					function setHeights() {
+
+						$.each(selectors, function() {
+
+							$el = $(this);
+							topPostion = $el.position().top;
+
+							if (currentRowStart != topPostion) {
+
+								for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+									rowDivs[currentDiv].height(currentTallest);
+								}
+
+								rowDivs.length = 0;
+								currentRowStart = topPostion;
+								currentTallest = $el.height();
+								rowDivs.push($el);
+
+							} else {
+								rowDivs.push($el);
+								currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+							}
+
+							for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+								rowDivs[currentDiv].height(currentTallest);
+							}
+
+						});
+					}
+
+					setHeights();
+
+				}
+			}
+
+
+		}
+
+})();
